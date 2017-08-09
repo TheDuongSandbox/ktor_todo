efceebec8b057d7d326d11722cd51154faadc366
Duong Tam Chau <dtc@iuservice.com>
2017-08-09 15:41:36 -0400

    Scaffold applicationa and a basic route.

---
+++ src/main/kotlin/com/iuservice/learning/kotlin/ktor/todo/TodoService.kt
@@ -0,0 +1,20 @@
+package com.iuservice.learning.kotlin.ktor.todo
+
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.application.call
+import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.features.DefaultHeaders
+import org.jetbrains.ktor.logging.CallLogging
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.Routing
+import org.jetbrains.ktor.routing.get
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
